5a0bafbfe314fa7c1c4530a2e2b890cc18e1439a
HenriqueLimas <henrique.ramos.limas@gmail.com>
2019-01-06 14:13:58 +0100

    0.2.0

--- src/elements/relative-time.js
+++ src/elements/relative-time.js
@@ -1,45 +1,17 @@
 import { define } from "osagai";
-import { onConnected, onDisconnected } from "osagai/lifecycles";
 import { useTime, observedAttributes } from "../useTime";
 import { toString } from "../relativeTime";
+import { scheduleFormatter } from "../scheduleFormatter";
 
 function RelativeTime({ element }) {
   useTime(element, getFormattedDate);
-
-  onConnected(element, function connectedCallback() {
-    nowElements.add(element);
-
-    if (!updateNowIntervalId) {
-      updateNowElements();
-
-      updateNowIntervalId = setInterval(updateNowElements, 60 * 1000);
-    }
-  });
-
-  onDisconnected(element, function disconnectedCallback() {
-    nowElements.delete(element);
-
-    if (!nowElements.size && updateNowIntervalId) {
-      clearInterval(updateNowIntervalId);
-      updateNowIntervalId = null;
-    }
-  });
+  scheduleFormatter(element, getFormattedDate);
 }
 
 function getFormattedDate(date) {
   if (date) {
     return toString(date);
   }
-}
-
-const nowElements = new Set();
-
-let updateNowIntervalId;
-
-function updateNowElements() {
-  nowElements.forEach(element => {
-    element.textContent = getFormattedDate(element.getDate());
-  });
 }
 
 define("relative-time", RelativeTime, { observedAttributes });
